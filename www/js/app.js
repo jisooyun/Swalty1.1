faf1e68a332747bf67e6b5cf070b4a1013725d2f
BertrandGuy <bertrand.guy1@gmail.com>
2016-06-11 23:58:28 +0200

    fix 1

--- www/js/app.js
+++ www/js/app.js
@@ -47,7 +47,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   })
 
       //PAGE SUCRE & SALE
-      .state('sucre', {
+     .state('sucre', {
         url: '/sucre',
         templateUrl: 'templates/sucre.html'
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -205,107 +205,107 @@ angular.module('starter.controllers', [])
 
 ])
 
-//FAVORIS CONTROLLER
-.controller('RecetteController', function($scope, Recettes, myService, updateFav){
+// //FAVORIS CONTROLLER
+// .controller('RecetteController', function($scope, Recettes, myService, updateFav){
 
-$scope.single = myService.get();
-  single = $scope.single;
+// $scope.single = myService.get();
+//   single = $scope.single;
 
 
-  //Add Favoris dans Users
-  var ref = new Firebase("https://swaltyapp.firebaseio.com/users");
-// Attach an asynchronous callback to read the data at our posts reference
-  ref.on("value", function(snapshot) {
-    //Hide btn
-    $scope.isFavoris = function(){
-    var auth = ref.getAuth();
-    var idUtilisateur = auth.uid;
-    var usersRef = ref.child(idUtilisateur);
-      var path = usersRef.toString();
-      var userRef = new Firebase(path);
-      userRef.on("value", function(snap){
-        var id = single.$id;
-        var tokenFavoris = 0
-        var banane = snap.val().fav;
-        for (var i = banane.length - 1; i >= 0; i--) {
-                if (banane[i] === id) {
-                    tokenFavoris = 1
-                };
-            }
-        if (tokenFavoris == 1) {
-            $scope.Favoris = {display : "none"}; 
-      }else{
-        $scope.delFavoris = {display : "none"}; 
-      }
-    });
-  }
+//   //Add Favoris dans Users
+//   var ref = new Firebase("https://swaltyapp.firebaseio.com/users");
+// // Attach an asynchronous callback to read the data at our posts reference
+//   ref.on("value", function(snapshot) {
+//     //Hide btn
+//     $scope.isFavoris = function(){
+//     var auth = ref.getAuth();
+//     var idUtilisateur = auth.uid;
+//     var usersRef = ref.child(idUtilisateur);
+//       var path = usersRef.toString();
+//       var userRef = new Firebase(path);
+//       userRef.on("value", function(snap){
+//         var id = single.$id;
+//         var tokenFavoris = 0
+//         var banane = snap.val().fav;
+//         for (var i = banane.length - 1; i >= 0; i--) {
+//                 if (banane[i] === id) {
+//                     tokenFavoris = 1
+//                 };
+//             }
+//         if (tokenFavoris == 1) {
+//             $scope.Favoris = {display : "none"}; 
+//       }else{
+//         $scope.delFavoris = {display : "none"}; 
+//       }
+//     });
+//   }
 
-    $scope.isFav = function(){
-        var auth = ref.getAuth();
-      var idUtilisateur = auth.uid;
+//     $scope.isFav = function(){
+//         var auth = ref.getAuth();
+//       var idUtilisateur = auth.uid;
 
-      var usersRef = ref.child(idUtilisateur);
-      var path = usersRef.toString();
-      console.log(ref)
-      var userRef = new Firebase(path);
-      userRef.on("value", function(snap){
-        var id = single.$id;
-        var tokenFav = 0
-        var banane = snap.val().fav;
-        for (var i = banane.length - 1; i >= 0; i--) {
-                if (banane[i] === id) {
-                    tokenFav = 1
-                };
-            }
-        if (tokenFav != 1) {
+//       var usersRef = ref.child(idUtilisateur);
+//       var path = usersRef.toString();
+//       console.log(ref)
+//       var userRef = new Firebase(path);
+//       userRef.on("value", function(snap){
+//         var id = single.$id;
+//         var tokenFav = 0
+//         var banane = snap.val().fav;
+//         for (var i = banane.length - 1; i >= 0; i--) {
+//                 if (banane[i] === id) {
+//                     tokenFav = 1
+//                 };
+//             }
+//         if (tokenFav != 1) {
             
-            banane.push(id);
-            updateFav.set(banane);
-        }else{
-            banane = snap.val().fav;
-            updateFav.set(banane);
-        }
-      })
-      banane = updateFav.get();
-      userRef.update({
-             fav : banane
-        });
+//             banane.push(id);
+//             updateFav.set(banane);
+//         }else{
+//             banane = snap.val().fav;
+//             updateFav.set(banane);
+//         }
+//       })
+//       banane = updateFav.get();
+//       userRef.update({
+//              fav : banane
+//         });
 
     
         
-        }
-    });
+//         }
+//     });
 
 
-  //Delete Favoris dans Users
-  var ref = new Firebase("https://swaltyapp.firebaseio.com/users");
-// Attach an asynchronous callback to read the data at our posts reference
-  ref.on("value", function(snapshot) {
-    $scope.delFav = function(){
-        var auth = ref.getAuth();
-        var idUtilisateur = auth.uid;
-        var usersRef = ref.child(idUtilisateur);
-        var path = usersRef.toString();
-        var userRef = new Firebase(path);
-        userRef.on("value", function(snap){
-            var banane = snap.val().fav;
-            var id = single.$id;
-            for (var i = banane.length - 1; i >= 0; i--) {
-                if (banane[i] === id) {
-                    banane.splice(i,1);
-                };
-            }
-            updateFav.set(banane);
-        })
-        banane = updateFav.get();
-        userRef.update({
-            fav : banane
-        });
+//   //Delete Favoris dans Users
+//   var ref = new Firebase("https://swaltyapp.firebaseio.com/users");
+// // Attach an asynchronous callback to read the data at our posts reference
+//   ref.on("value", function(snapshot) {
+//     $scope.delFav = function(){
+//         var auth = ref.getAuth();
+//         var idUtilisateur = auth.uid;
+//         var usersRef = ref.child(idUtilisateur);
+//         var path = usersRef.toString();
+//         var userRef = new Firebase(path);
+//         userRef.on("value", function(snap){
+//             var banane = snap.val().fav;
+//             var id = single.$id;
+//             for (var i = banane.length - 1; i >= 0; i--) {
+//                 if (banane[i] === id) {
+//                     banane.splice(i,1);
+//                 };
+//             }
+//             updateFav.set(banane);
+//         })
+//         banane = updateFav.get();
+//         userRef.update({
+//             fav : banane
+//         });
 
     
         
-  }
+//   }
     
 
-    });
-});
+//     });
+// });
